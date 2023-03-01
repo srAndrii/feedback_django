274c330815e7278d8727ee1557532a7766e57089
srAndrii <55akif5548@gmail.com>
2023-03-01 20:18:11 +0200

    Manual Form Validation

--- reviews/views.py
+++ reviews/views.py
@@ -7,10 +7,17 @@ from django.http import HttpResponseRedirect
 def review(request):
     if request.method == 'POST':
         entered_username = request.POST['username']
+
+        if entered_username == "" and len(entered_username) >= 100:
+            return render(request, "reviews/review.html", {
+                "has_error": True
+            })
         print(entered_username)
         return HttpResponseRedirect("/thank-you")
 
-    return render(request, "reviews/review.html")
+    return render(request, "reviews/review.html", {
+        "has_error": False
+    })
 
 
 def thank_you(request):
