81d90923484e6116af17358f32f821d265541ecf
srAndrii <55akif5548@gmail.com>
2023-03-06 16:54:06 +0200

    TemplateView

--- reviews/views.py
+++ reviews/views.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from django.views import View
+from django.views.generic.base import TemplateView
 
 from .form import ReviewForm
 
@@ -21,11 +22,16 @@ class ReviewView(View):
         if form.is_valid():
             form.save()
             return HttpResponseRedirect("/thank-you")
-        
+
         return render(request, "reviews/review.html", {
             "form": form
         })
 
 
-def thank_you(request):
-    return render(request, "reviews/thank_you.html")
+class ThankYouView(TemplateView):
+    template_name = "reviews/thank_you.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["message"] = "This works!"
+        return context
