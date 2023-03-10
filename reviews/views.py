47487224838867cd87654f40c63ffea8699ce0fb
srAndrii <55akif5548@gmail.com>
2023-03-10 09:10:10 +0200

    FormView

--- reviews/views.py
+++ reviews/views.py
@@ -4,6 +4,7 @@ from django.shortcuts import render
 from django.views import View
 from django.views.generic.base import TemplateView
 from django.views.generic import ListView, DetailView
+from django.views.generic.edit import FormView
 
 from .form import ReviewForm
 from .models import Review
@@ -11,24 +12,33 @@ from .models import Review
 # Create your views here.
 
 
-class ReviewView(View):
-    def get(self, request):
-        form = ReviewForm()
+# class ReviewView(View):
+#     def get(self, request):
+#         form = ReviewForm()
 
-        return render(request, "reviews/review.html", {
-            "form": form
-        })
+#         return render(request, "reviews/review.html", {
+#             "form": form
+#         })
 
-    def post(self, request):
-        form = ReviewForm(request.POST)
+#     def post(self, request):
+#         form = ReviewForm(request.POST)
 
-        if form.is_valid():
-            form.save()
-            return HttpResponseRedirect("/thank-you")
+#         if form.is_valid():
+#             form.save()
+#             return HttpResponseRedirect("/thank-you")
 
-        return render(request, "reviews/review.html", {
-            "form": form
-        })
+#         return render(request, "reviews/review.html", {
+#             "form": form
+#         })
+
+class ReviewView(FormView):
+    form_class = ReviewForm
+    template_name = "reviews/review.html"
+    success_url = "/thank-you"
+
+    def form_valid(self, form):
+        form.save()
+        return super().form_valid(form)
 
 
 class ThankYouView(TemplateView):
